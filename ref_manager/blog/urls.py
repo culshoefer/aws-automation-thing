6fc4a206d9e35e5de104f48d7c1d972ae29e4f5c
abdulfaizp <abdulfaizp@gmail.com>
2016-11-01 12:35:18 +0000

    url changes

--- ref_manager/blog/urls.py
+++ ref_manager/blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.login_request, name='login'),
-    url(r'^references/$', views.references, name='references'),
+    url(r'^home/$', views.home, name='references'),
+    url(r'^references/$', views.references),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^team/$', views.group, name='group'),
     url(r'^authenticate/$', views.authentication),
