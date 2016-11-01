4a93503f6ddf1603b02081de7494ea375b5fbc48
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2016-11-01 13:43:52 +0000

    Added first Travis script. TODO test it

---
+++ djangosetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Now installing all Python tools"
+
+function virtualenv_dep_install {
+    cd ref_manager
+    if [ ! -x venv/bin/pip ]; then
+	virtualenv venv
+    fi
+    venv/bin/python venv/bin/pip install -r requirements.txt
+}
+
+function virtualenv_install {
+    echo "Installing virtualenv, it may ask for SUDO password"
+    sudo pip install virtualenv
+}
+
+if [ ! -z `which virtualenv` ]; then
+    virtualenv_install
+fi
