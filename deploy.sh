d0354e97eb26cd74807ca2dd769e0af7ff6adb08
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2016-11-03 18:30:51 +0000

    fix 3

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,10 @@ ls
 cd aws-automation-thing
 echo "Now running ssh agent"
 eval `ssh-agent -s`
-#echo "$DEPLOY_KEY" > deploy_key.pem
-#chmod 600 deploy_key.pem
+echo "$DEPLOY_KEY" > ~/.ssh/deploy_key.pem
+chmod 600 ~/.ssh/deploy_key.pem
 echo "Now adding key"
-
-sudo apt-get install expect
-
-expect << EOF
-  spawn ssh-add $DEPLOY_KEY
-  expect "Enter passphrase"
-  send "\r"
-  expect eof
-EOF
-#sh-add -p deploy_key.pem
+ssh-add ~/.ssh/deploy_key.pem
 echo "Now adding remote"
 git remote add live ssh://ubuntu@ec2-54-229-70-158.eu-west-1.compute.amazonaws.com/home/ubuntu/repo/site.git
 echo "Now pushing"
