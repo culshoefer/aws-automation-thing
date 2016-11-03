0c1f8076f37350287e589b601f13474924618a23
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2016-11-03 23:05:02 +0000

    Fixed indent

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,12 @@ env.password = 'XXXXXXXX'
 
 
 def deploy():
-	path = '~/aws-automation-thing'
-    run('cd %(path)s; git pull' % {'path' : path})
-    run('source venv/bin/activate')
-    run('cd ref-manager')
-    run('python manage.py makemigrations')
-    run('python manage.py migrate')
-    run('python manage collect_static')
-    sudo('stystemclt restart nginx')
+        path = '~/aws-automation-thing'
+        run('cd %(path)s; git pull' % {'path' : path})
+        run('source venv/bin/activate')
+        run('cd ref-manager')
+        run('python manage.py makemigrations')
+        run('python manage.py migrate')
+        run('python manage collect_static')
+        sudo('stystemclt restart nginx')
 	print('deploy complete!')
