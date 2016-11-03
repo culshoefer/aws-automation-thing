e6e201aa0b171e0beb6e40e77541e6b30fd0e284
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2016-11-03 18:50:07 +0000

    Fix 7

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ ls
 cd aws-automation-thing
 echo "Now running ssh agent"
 eval "$(ssh-agent -s)"
-echo "$DEPLOY_KEY" > .travis/deploy_key.pem
-chmod 600 .travis/deploy_key.pem
+echo "$DEPLOY_KEY" > deploy_key.pem
+chmod 600 deploy_key.pem
 echo "Now adding key"
-ssh-add .travis/deploy_key.pem
+ssh-add deploy_key.pem
 echo "Now adding remote"
 git remote add live ssh://ubuntu@ec2-54-229-70-158.eu-west-1.compute.amazonaws.com/home/ubuntu/repo/site.git
 echo "Now pushing"
