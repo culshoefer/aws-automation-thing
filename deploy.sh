06720db19f5c52ac3b3659051f25dbee5ec1b369
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2016-11-03 17:45:00 +0000

    Changed to git init bare system

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#eval "$(ssh-agent -s)"
-#echo "$DEPLOY_KEY" > deploy_key.pem
-#chmod 600 deploy_key.pem
-#ssh-add deploy_key.pem
-git remote add live ssh://ubuntu@ec2-54-229-70-158.eu-west-1.compute.amazonaws.com/repo/site.git
+cd
+cd aws-automation-thing
+ssh-agent -s
+echo "$DEPLOY_KEY" > deploy_key.pem
+chmod 600 deploy_key.pem
+ssh-add deploy_key.pem
+git remote add live ssh://ubuntu@ec2-54-229-70-158.eu-west-1.compute.amazonaws.com/home/ubuntu/repo/site.git
 git push live master
--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-zip -r latest * --quiet
-mkdir -p rdy4deploy_ref_manager
-mv latest.zip rdy4deploy_ref_manager/latest.zip
+#zip -r latest * --quiet
+#mkdir -p rdy4deploy_ref_manager
+#mv latest.zip rdy4deploy_ref_manager/latest.zip
