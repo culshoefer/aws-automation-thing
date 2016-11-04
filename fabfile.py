1234db28a573fc3d86db0dc96e4a7be342f8b307
Anirudh <anirudhx5@gmail.com>
2016-11-04 00:37:56 +0000

    setup in fab file

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,13 @@ env.hosts = ["54.229.146.21"]
 env.user = "ubuntu"
 env.key_filename = "./scenario.pem"
 
+def setup():
+    run('cd /home/ubuntu/aws-automatic-thing && ./djangosetup.sh')
+    run('cd /home/ubuntu/aws-automatic-thing && ./serverscripts/serversetup.sh')
+    run('cd /home/ubuntu/aws-automatic-thing && ./djangomigrations.sh')
 
 def deploy():
     run('cd /home/ubuntu/aws-automation-thing && git pull')
     run('cd /home/ubuntu/aws-automation-thing && ./update.sh')
+    setup()
     print('deployed')
-
